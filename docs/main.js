31387024cbb3817707d543830c4c7c2c9d7c3f17
Tomasz Goralczyk <devflag@gmail.com>
2017-03-23 12:27:32 -0400

    Added a strip with navigation dots that changes as user scrolls up and down the page

--- docs/main.js
+++ docs/main.js
@@ -13,14 +13,6 @@ function createLink(href, innerHTML, className) {
   link.className = className;
 
   link.onclick = function (event) {
-    // const select = `#${href}`;
-    // console.log(select);
-    // console.log(document.getElementById(href));
-    // document.getElementById.scrollIntoView({ behavior: 'smooth' });
-    // document.querySelector(select).scrollIntoView({ behavior: 'smooth' });
-    // document.querySelector('#\\4').scrollIntoView({ behavior: 'smooth' });
-    // console.log(document.querySelector('[id="4"]').scrollIntoView({ behavior: 'smooth' }));
-    // console.log(window.scrollBy());
     document.querySelector('[id="' + href + '"]').scrollIntoView({ behavior: 'smooth' });
   };
 
@@ -43,103 +35,65 @@ function createBottomPaginationFor(sections) {
   }
 }
 
+function createLinkForSideNavigation(href) {
+  var link = document.createElement('a');
+
+  link.href = '#' + href;
+
+  return link;
+}
+
+function createSidePaginationFor(sections) {
+  var pager = document.getElementById('pager');
+
+  var _loop = function _loop(i) {
+    var curr = i + 1;
+    var listItem = document.createElement('li');
+
+    listItem.appendChild(createLinkForSideNavigation(curr));
+    pager.appendChild(listItem);
+
+    listItem.onclick = function () {
+      document.querySelector('[id="' + curr + '"]').scrollIntoView({ behavior: 'smooth' });
+    };
+  };
+
+  for (var i = 0; i < sections.length; i++) {
+    _loop(i);
+  }
+}
+
 window.onload = function init() {
-  console.log('Hello');
-
   var sections = getAllSections();
-  createBottomPaginationFor(sections);
-};
+  var sectionsPositions = [];
+  var pager = document.getElementById('pager');
 
-// (() => {
-//   'use strict';
-//
-//   let winY,
-//       winH,
-//       slideSpeed = 400;
-//   const $pager = $('#pager');
-//   const $section = $('.section');
-//
-//   // Elements Array
-//   let sectionArray = [];
-//   let currentSectionArray = [];
-//   $section.each((idx) => {
-//     currentSectionArray.push('');
-//     sectionArray.push('');
-//     sectionArray[idx] = $(this).offset();
-//     // sectionArray[idx] = $(this).offset().top;
-//   });
-//
-//   // Populate Dynamic Pager
-//   $section.each((idx) => {
-//     // const tooltip = $section[idx];
-//     $pager.append(`<li><a href="#${idx}"><span>${idx}</span></a></li>`);
-//   });
-//
-//   // Populate Section Navigation
-//   $section.each((idx) => {
-//     const btnWrap = $(this).find('.btn-wrap');
-//
-//     if (idx === 0) {
-//       btnWrap.append('<a href="#" class="btn btn-next">Next &#8594;</a>');
-//     } else if (idx === $section.length - 1) {
-//       btnWrap.append('<a href="#" class="btn btn-prev">&#8592; Previous</a>');
-//     } else {
-//       btnWrap.append('<a href="#" class="btn btn-prev">&#8592; Previous</a><a href="#" class="btn btn-next">Next &#8594;</a>');
-//     }
-//   });
-//
-//   $('body').append('<div id="console"></div>');
-//
-//   // Update Data
-//   function updateConsole() {
-//     winH = Math.ceil($(window).innerHeight());
-//     winY = Math.ceil($('body').scrollTop());
-//
-//     $section.each((idx) => {
-//       currentSectionArray[idx] = winY - $(this).offset().top;
-//
-//       if (currentSectionArray[idx] < -(winH/2) || currentSectionArray[idx] >= winH) {
-//         currentSectionArray[index] = Math.ceil(winY - $(this).offset().top);
-//       } else if (currentSectionArray[idx] === 0) {
-//         currentSectionArray[idx] = 'current';
-//       } else {
-//         currentSectionArray[idx] = 'visible';
-//         $('#pager li').removeAttr('class');
-//         index++;
-//         $pager.find(`li:nth-child('${idx}')`).addClass('active');
-//       }
-//     });
-//
-//     $('#console').html(`${winH} / ${winY} / [${currentSectionArray}]`);
-//   }
-//   updateConsole();
-//
-//   // On Resize
-//   $(window).on('resize scroll', () => {
-//     updateConsole();
-//   });
-//
-//   // Pager Navigation
-//   $pager.find('a').on('click', (event) => {
-//     event.preventDefault();
-//     const pagerIndex = $(this).closest('li').index();
-//     $('html', 'body').animate({
-//       scrollTop: sectionArray[pagerIndex]
-//     }, slideSpeed);
-//   });
-//
-//   // In-slide Navigation
-//   $('.box .btn').on('click', (event) => {
-//     event.preventDefault();
-//     if ($(this).hasClass('btn-prev')) {
-//       $('html, body').animate({
-//         scrollTop: sectionArray[$(this).closest('.section').index() - 1]
-//       }, slideSpeed);
-//     } else if ($(this).hasClass('btn-next')) {
-//       $('html, body').animate({
-//         scrollTop: sectionArray[$(this).closest('.section').index() + 1]
-//       }, slideSpeed);
-//     }
-//   });
-// })();
+  createBottomPaginationFor(sections);
+  createSidePaginationFor(sections);
+
+  for (var i = 0; i < sections.length; i++) {
+    var pos = sections[i].getBoundingClientRect();
+    sectionsPositions.push(pos.top);
+  }
+
+  pager.children[0].className = 'active';
+
+  window.onscroll = function () {
+
+    var found = sectionsPositions.find(function (sectionPosition) {
+      return sectionPosition === window.pageYOffset;
+    });
+
+    if (found >= 0) {
+      var idx = sectionsPositions.indexOf(found);
+      var listItems = pager.children;
+
+      for (var _i = 0; _i < pager.children.length; _i++) {
+        pager.children[_i].className = '';
+      }
+
+      listItems[idx].className = 'active';
+    }
+  };
+};
 //# sourceMappingURL=main.js.map
--- src/main.js
+++ src/main.js
@@ -11,14 +11,6 @@ function createLink(href, innerHTML, className) {
   link.className = className;
 
   link.onclick = (event) => {
-    // const select = `#${href}`;
-    // console.log(select);
-    // console.log(document.getElementById(href));
-    // document.getElementById.scrollIntoView({ behavior: 'smooth' });
-    // document.querySelector(select).scrollIntoView({ behavior: 'smooth' });
-    // document.querySelector('#\\4').scrollIntoView({ behavior: 'smooth' });
-    // console.log(document.querySelector('[id="4"]').scrollIntoView({ behavior: 'smooth' }));
-    // console.log(window.scrollBy());
     document.querySelector(`[id="${href}"]`).scrollIntoView({ behavior: 'smooth' });
   };
 
@@ -49,103 +41,60 @@ function createBottomPaginationFor(sections) {
   }
 }
 
+function createLinkForSideNavigation(href) {
+  const link = document.createElement('a');
+
+  link.href = `#${href}`;
+
+  return link;
+}
+
+function createSidePaginationFor(sections) {
+  const pager = document.getElementById('pager');
+
+  for (let i = 0; i < sections.length; i++) {
+    const curr = i + 1;
+    const listItem = document.createElement('li');
+
+    listItem.appendChild(createLinkForSideNavigation(curr));
+    pager.appendChild(listItem);
+
+    listItem.onclick = () => {
+      document.querySelector(`[id="${curr}"]`).scrollIntoView({ behavior: 'smooth' });
+    };
+  }
+}
+
 window.onload = function init() {
-  console.log('Hello');
-
   const sections = getAllSections();
+  const sectionsPositions = [];
+  const pager = document.getElementById('pager');
+
   createBottomPaginationFor(sections);
+  createSidePaginationFor(sections);
+
+  for (let i = 0; i < sections.length; i++) {
+    const pos = sections[i].getBoundingClientRect();
+    sectionsPositions.push(pos.top);
+  }
+
+  pager.children[0].className='active';
+
+  window.onscroll = () => {
+
+    const found = sectionsPositions.find((sectionPosition) => {
+      return sectionPosition === window.pageYOffset;
+    });
+
+    if (found >= 0) {
+      const idx = sectionsPositions.indexOf(found);
+      const listItems = pager.children;
+
+      for (let i = 0; i < pager.children.length; i++) {
+        pager.children[i].className = '';
+      }
+
+      listItems[idx].className = 'active';
+    }
+  };
 };
-
-
-// (() => {
-//   'use strict';
-//
-//   let winY,
-//       winH,
-//       slideSpeed = 400;
-//   const $pager = $('#pager');
-//   const $section = $('.section');
-//
-//   // Elements Array
-//   let sectionArray = [];
-//   let currentSectionArray = [];
-//   $section.each((idx) => {
-//     currentSectionArray.push('');
-//     sectionArray.push('');
-//     sectionArray[idx] = $(this).offset();
-//     // sectionArray[idx] = $(this).offset().top;
-//   });
-//
-//   // Populate Dynamic Pager
-//   $section.each((idx) => {
-//     // const tooltip = $section[idx];
-//     $pager.append(`<li><a href="#${idx}"><span>${idx}</span></a></li>`);
-//   });
-//
-//   // Populate Section Navigation
-//   $section.each((idx) => {
-//     const btnWrap = $(this).find('.btn-wrap');
-//
-//     if (idx === 0) {
-//       btnWrap.append('<a href="#" class="btn btn-next">Next &#8594;</a>');
-//     } else if (idx === $section.length - 1) {
-//       btnWrap.append('<a href="#" class="btn btn-prev">&#8592; Previous</a>');
-//     } else {
-//       btnWrap.append('<a href="#" class="btn btn-prev">&#8592; Previous</a><a href="#" class="btn btn-next">Next &#8594;</a>');
-//     }
-//   });
-//
-//   $('body').append('<div id="console"></div>');
-//
-//   // Update Data
-//   function updateConsole() {
-//     winH = Math.ceil($(window).innerHeight());
-//     winY = Math.ceil($('body').scrollTop());
-//
-//     $section.each((idx) => {
-//       currentSectionArray[idx] = winY - $(this).offset().top;
-//
-//       if (currentSectionArray[idx] < -(winH/2) || currentSectionArray[idx] >= winH) {
-//         currentSectionArray[index] = Math.ceil(winY - $(this).offset().top);
-//       } else if (currentSectionArray[idx] === 0) {
-//         currentSectionArray[idx] = 'current';
-//       } else {
-//         currentSectionArray[idx] = 'visible';
-//         $('#pager li').removeAttr('class');
-//         index++;
-//         $pager.find(`li:nth-child('${idx}')`).addClass('active');
-//       }
-//     });
-//
-//     $('#console').html(`${winH} / ${winY} / [${currentSectionArray}]`);
-//   }
-//   updateConsole();
-//
-//   // On Resize
-//   $(window).on('resize scroll', () => {
-//     updateConsole();
-//   });
-//
-//   // Pager Navigation
-//   $pager.find('a').on('click', (event) => {
-//     event.preventDefault();
-//     const pagerIndex = $(this).closest('li').index();
-//     $('html', 'body').animate({
-//       scrollTop: sectionArray[pagerIndex]
-//     }, slideSpeed);
-//   });
-//
-//   // In-slide Navigation
-//   $('.box .btn').on('click', (event) => {
-//     event.preventDefault();
-//     if ($(this).hasClass('btn-prev')) {
-//       $('html, body').animate({
-//         scrollTop: sectionArray[$(this).closest('.section').index() - 1]
-//       }, slideSpeed);
-//     } else if ($(this).hasClass('btn-next')) {
-//       $('html, body').animate({
-//         scrollTop: sectionArray[$(this).closest('.section').index() + 1]
-//       }, slideSpeed);
-//     }
-//   });
-// })();
